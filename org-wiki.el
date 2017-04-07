f5004dcc2fa7ed13b8f4d435caa6e603b31c03cd
CaioRS <caiorss.rodrigues@gmail.com>
2017-04-07 16:06:29 -0300

    Update version number to 3.2.

--- org-wiki.el
+++ org-wiki.el
@@ -5,7 +5,7 @@
 ;; Author: Caio Rodrigues       <caiorss DOT rodrigues AT gmail DOT com>
 ;; Maintainer: Caio Rordrigues  <caiorss DOT rodrigues AT gmail DOT com>
 ;; Keywords: org-mode, wiki, notes, notebook
-;; Version: 3.1
+;; Version: 3.2
 ;; URL: https://www.github.com/caiorss/org-wiki'
 ;; Package-Requires: ((helm-core "2.0") (org "9") (cl-lib "0.5"))
 
