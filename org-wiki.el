0c864a349fc71eeb8a2c7905107280edd432f68b
CaioRS <caiorss.rodrigues@gmail.com>
2017-07-12 04:12:38 -0300

    Change command org-wiki-link for org-wiki-create.
     - The comamnd org-wiki-link was renamed as org-wiki-create because it
       easy to remember and describes better its action.

--- org-wiki.el
+++ org-wiki.el
@@ -691,8 +691,8 @@ to cancel the download."
   (org-wiki--helm-selection
    (lambda (page) (insert (org-wiki--make-link page)))))
 
-(defun org-wiki-link ()
-  "Insert a wiki link at point. Useful to create new pages."
+(defun org-wiki-create ()
+  "Create a new org-wiki page asking the user its name."
   (interactive)
   (let ((page-name (read-string  "Page: ")))
     (save-excursion (insert (org-make-link-string (concat "wiki:" page-name)
